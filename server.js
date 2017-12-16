693c4fcdc25bbb59c17b8ae127a973c18a6e28c8
idah <idir.dahmouh@gmail.com>
2017-12-16 16:59:21 +0100

    Add semantic css framework

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-let app = require('express')()
+let express = require('express')
+
+let app = express()
 
 app.set('view engine', 'ejs')
 
+app.use('/assets', express.static('public'))
 
 app.get('/', (request, response) => {
   response.render('pages/index', {test: 'Hello world'})
